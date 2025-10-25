a3dd3f8a0e0e4123390d730981ec8ceffb84552e
Baphomet <zloostpvp@gmail.com>
2025-10-25 13:20:32 -0300

    add 1. BruteForce

---
+++ leetcode/Solutions/CS/0001-0050/_1-BruteForce.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Solutions.CS._0001_0050
+{
+    internal class _1_BruteForce
+    {
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                int[] result = new int[2];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (j == i)
+                        {
+                            j++;
+                        }
+                        else if (nums[i] + nums[j] == target)
+                        {
+                            result[0] = j;
+                            result[1] = i;
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
